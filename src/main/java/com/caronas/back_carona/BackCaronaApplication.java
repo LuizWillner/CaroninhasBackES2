212421e526a11619724612ec99113123c54cc3a3
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2024-04-01 17:38:28 -0300

    Adicionando a estrutura spring

---
+++ src/main/java/com/caronas/back_carona/BackCaronaApplication.java
@@ -0,0 +1,13 @@
+package com.caronas.back_carona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCaronaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCaronaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caronas/back_carona/BackCaronaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caronas.back_carona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCaronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
