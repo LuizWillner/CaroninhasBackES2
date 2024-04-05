7bd17d24f5cd8a99fa0f81ab624c6289324c1f2a
LuizWillner <luizwillner@gmail.com>
2024-04-05 15:10:01 -0300

    Tabelas Driver, DriverVehicle e Vehicle criadas

--- app/database/user_orm.py
+++ app/database/user_orm.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean, UniqueConstraint
 from database import Base
 
 
@@ -12,8 +14,51 @@ class User(Base):
     last_name = Column(String, index=False, nullable=False)
     cpf = Column(String, index=True, nullable=False, unique=True)
     iduff = Column(String, index=True, nullable=True, unique=True)
-    password = Column(String, index=False, nullable=True)
+    hashed_password = Column(String, index=False, nullable=False)
     birthdate = Column(DateTime, index=False, nullable=False)
-    created_at = Column(DateTime, index=False, nullable=False, default=datetime.now)
+    created_at = Column(DateTime, index=False, nullable=False, server_default=func.now())
     active = Column(Boolean, index=False, nullable=False, default=True)
     
+    driver = relationship('Driver', lazy=False, uselist=False, back_populates="user", cascade="all, delete")
+    
+    
+class Driver(Base):
+    __tablename__ = "driver"
+    
+    id  = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    fk_user = Column(Integer, ForeignKey("user.id"), unique=True, index=True)
+    license = Column(String, index=True, nullable=False, unique=True)
+    created_at = Column(DateTime, index=False, nullable=False, server_default=func.now())
+    
+    user = relationship('User', lazy=False, uselist=False, back_populates="driver")
+    driver_vehicle = relationship("DriverVehicle", lazy=True, uselist=True, back_populates="driver", cascade="all, delete")
+    
+
+class DriverVehicle(Base):
+    __tablename__ = "driver_vehicle"
+    
+    id  = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    fk_driver = Column(Integer, ForeignKey("driver.id"), index=True)
+    fk_vehicle = Column(Integer, ForeignKey("vehicle.id"), index=True)
+    plate = Column(String, index=True, nullable=False, unique=True)
+    created_at = Column(DateTime, index=False, nullable=False, server_default=func.now())
+    
+    driver = relationship("Driver", lazy=True, uselist=False, back_populates="driver_vehicle")
+    vehicle = relationship("Vehicle", lazy=True, uselist=False, back_populates="driver_vehicle")
+    
+
+class Vehicle(Base):
+    __tablename__ = "vehicle"
+    
+    id  = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    type = Column(String, index=True, nullable=False, unique=False)
+    brand = Column(String, index=True, nullable=False, unique=False)
+    model = Column(String, index=True, nullable=False, unique=False)
+    color = Column(String, index=False, nullable=True, unique=False)
+    created_at = Column(DateTime, index=False, nullable=False, server_default=func.now())
+    
+    __table_args__ = (
+        UniqueConstraint("type", "brand", "model", "color"),
+    )
+    
+    driver_vehicle = relationship("DriverVehicle", lazy=True, uselist=True, back_populates="vehicle")
