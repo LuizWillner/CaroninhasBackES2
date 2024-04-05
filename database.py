0ccc87873bed9851548048dfc25dd95c9d75a593
LuizWillner <luizwillner@gmail.com>
2024-04-05 12:30:26 -0300

    engine e SessionLocal do SQLAlch inicializados

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+SQLALCHEMY_DATABASE_URL = os.environ.get("DB_URI")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-from app.database.user_orm import User
-
+from app.database.user_orm import *
