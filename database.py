c3524379ae6ac28ade4acc197dcd848329d05b0a
LuizWillner <luizwillner@gmail.com>
2024-04-05 16:34:06 -0300

    FIX: Erro ao carregar envar com url do database

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+load_dotenv(dotenv_path="credentials.env")
 SQLALCHEMY_DATABASE_URL = os.environ.get("DB_URI")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
