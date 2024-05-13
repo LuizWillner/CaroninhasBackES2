34371ad9d9286d4761b3c3737944e17e19a50ef1
LuizWillner <luizwillner@gmail.com>
2024-05-13 12:00:10 -0300

    REFACTOR: vehicle_orm.py renomeado para veiculo_orm.py

--- database.py
+++ database.py
@@ -12,4 +12,4 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 from app.database.user_orm import *
-from app.database.vehicle_orm import *
+from app.database.veiculo_orm import *
