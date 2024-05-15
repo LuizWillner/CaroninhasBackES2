339373a4ae8c5daa198348714c41885275d0d93c
LuizWillner <luizwillner@gmail.com>
2024-05-15 00:37:25 -0300

    REFACTOR: param current_motorista renomeado para motorista na func create_carona()

--- app/routers/carona.py
+++ app/routers/carona.py
@@ -12,7 +12,7 @@ from app.models.carona_oop import CaronaBase, CaronaExtended
 from app.core.db_utils import get_db
 from app.core.carona import add_carona_to_db
 from app.core.motorista import get_current_active_motorista
-from app.core.veiculo import get_veiculo_motorista_by_id
+from app.core.veiculo import get_motorista_veiculo_of_user
 from app.models.router_tags import RouterTags
 
 
@@ -21,8 +21,8 @@ router = APIRouter(prefix="/carona", tags=[RouterTags.carona])
 
 @router.post("", response_model=CaronaExtended)
 def create_carona(
-    current_motorista: Annotated[Motorista, Depends(get_current_active_motorista)],
-    motorista_veiculo: Annotated[MotoristaVeiculo, Depends(get_veiculo_motorista_by_id)],
+    motorista: Annotated[Motorista, Depends(get_current_active_motorista)],
+    motorista_veiculo: Annotated[MotoristaVeiculo, Depends(get_motorista_veiculo_of_user)],
     veiculo_id: int,
     hora_de_partida: datetime,
     preco_carona: float,
@@ -32,7 +32,7 @@ def create_carona(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Vehicle of current user with id {veiculo_id} not found.")
     carona = add_carona_to_db(
         carona_to_add=CaronaBase(
-            fk_motorista=current_motorista.id_fk_user,
+            fk_motorista=motorista.id_fk_user,
             fk_motorista_veiculo=motorista_veiculo.id,
             hora_partida= hora_de_partida,
             valor=preco_carona
