4d0cad8725aa8994b0022547573e9e32879bd16f
LuizWillner <luizwillner@gmail.com>
2024-05-23 15:45:41 -0300

    FEAT: Colunas de local de partida e chegada adicionadas nas tabelas Carona e PedidoCarona

--- app/database/carona_orm.py
+++ app/database/carona_orm.py
@@ -13,6 +13,8 @@ class Carona(Base):
     fk_motorista_veiculo = Column(Integer, ForeignKey("motorista_veiculo.id"), index=True, nullable=False)
     valor = Column(Float, index=True, nullable=False)
     hora_partida = Column(DateTime, index=True, nullable=False)
+    local_partida = Column(String, index=True, nullable=False)
+    local_chegada = Column(String, index=True, nullable=False)
     created_at = Column(DateTime, index=False, nullable=False, default=datetime.now)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.current_timestamp())
     
