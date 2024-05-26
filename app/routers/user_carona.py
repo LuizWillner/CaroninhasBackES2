246066b5a1b5f1d8810233d2dc5d47bbff85adcc
LuizWillner <luizwillner@gmail.com>
2024-05-26 15:11:27 -0300

    FIX: Motorista não pode mais se inscrever na própria carona

--- app/routers/user_carona.py
+++ app/routers/user_carona.py
@@ -35,7 +35,7 @@ def add_me_to_carona(
             fk_user=current_user.id,
             fk_carona=carona_id,
         ),
-        total_vagas_carona=carona.vagas,
+        carona=carona,
         db=db
     )
     return user_carona
