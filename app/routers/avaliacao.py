389ad5177168c4c8262771f24d0485e988145ba8
LuizWillner <luizwillner@gmail.com>
2024-06-03 18:21:28 -0300

    FIX: Endpoint de avaliação de passageiro

--- app/routers/avaliacao.py
+++ app/routers/avaliacao.py
@@ -9,7 +9,7 @@ from app.core.authentication import get_current_active_user
 from app.database.user_carona_orm import UserCarona
 from app.database.carona_orm import Carona
 
-from app.models.user_carona_oop import UserCaronaWithUser
+from app.models.user_carona_oop import UserCaronaModel, UserCaronaWithUser
 from app.models.avaliacao_opp import AvaliacaoMotorista, AvaliacaoPassageiro
 
 
@@ -19,67 +19,42 @@ from app.database.user_orm import User
 
 router = APIRouter(prefix="/avaliacao", tags=[RouterTags.avaliacao])
 
-@router.post("/motorista",response_model=UserCaronaWithUser, description="Notas de 1 a 5 para avaliação")
-def teste_avaliacao_motorista(
+
+@router.post("/passageiro",response_model=UserCaronaModel, description="Notas de 1 a 5 para avaliação")
+def avaliacao_passageiro(
     db: Annotated[Session, Depends(get_db)],
     current_user: Annotated[User, Depends(get_current_active_user)],
     carona_id: int,
-    avaliacao: AvaliacaoMotorista
-) -> UserCaronaWithUser:
+    user_avaliado_id: float,
+    avaliacao: AvaliacaoPassageiro
+) -> UserCaronaModel:
+
+    carona = db.query(Carona).filter(Carona.id == carona_id, Carona.fk_motorista == current_user.id).first()
+    if not carona:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Carona não encontrada")
     
+    user_carona: UserCarona = db.query(UserCarona).filter(UserCarona.fk_carona == carona.id, UserCarona.fk_user == user_avaliado_id).first()
+    if not user_carona:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado na carona.")
     
-    db = db
-    carona = db.query(Carona).filter(Carona.fk_motorista_veiculo== carona.id, Carona.fk_motorista == current_user.id).first()# faz query da carona passando o id
-    user_carona = db.query(UserCarona).filter(UserCarona.fk_carona == carona.id, UserCarona.fk_user == current_user.id).first()
+    if avaliacao.nota_passageiro > 5 or avaliacao.nota_passageiro < 1:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Valor da avaliação deve ser entre 1 e 5")
     
-    user_carona.nota_motorista = avaliacao.nota_motorista
-    user_carona.comentário_sobre_motorista = avaliacao.comentário_motorista
-    
-    if avaliacao.nota_motorista > 5 or avaliacao.nota_motorista < 1:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Valor deve ser entre 1 e 5")
-     
-    try:
-        db.add(carona)
-        db.commit()
-    except exc.SQLAlchemyError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Erro ao criar avaliacao")
+    user_carona.nota_pasageiro = avaliacao.nota_passageiro
+    user_carona.comentário_sobre_passageiro = avaliacao.comentario_passageiro
     
     try:
         db.add(user_carona)
         db.commit()
     except exc.SQLAlchemyError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Erro ao criar avaliação")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Erro ao criar avaliação sobre passageiro id={user_avaliado_id}")
     
     return user_carona
 
-@router.post("/passageiro",response_model=UserCaronaWithUser, description="Notas de 1 a 5 para avaliação")
-def teste_avaliacao_passageiro(
-    db: Annotated[Session, Depends(get_db)],
-    current_user: Annotated[User, Depends(get_current_active_user)],
-    carona_id: int,
-    avaliacao: AvaliacaoPassageiro
-) -> UserCaronaWithUser:
+
+@router.post("/motorista",response_model=UserCaronaModel, description="Notas de 1 a 5 para avaliação")
+def avaliacao_motorista(
     
-    db = db
-    carona = db.query(Carona).filter(Carona.fk_motorista_veiculo== carona.id, Carona.fk_motorista == current_user.id).first()# faz query da carona passando o id
-    user_carona = db.query(UserCarona).filter(UserCarona.fk_carona == carona.id, UserCarona.fk_user == current_user.id).first()
-    
-    user_carona.nota_motorista = avaliacao.nota_motorista
-    user_carona.comentário_sobre_motorista = avaliacao.comentário_motorista
-    
-    if avaliacao.nota_passageiro > 5 or avaliacao.nota_passageiro < 1:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="Valor deve ser entre 1 e 5")
-    
-    try:
-        db.add(carona)
-        db.commit()
-    except exc.SQLAlchemyError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Erro ao criar avaliacao")
-    
-    try:
-        db.add(user_carona)
-        db.commit()
-    except exc.SQLAlchemyError:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Erro ao criar avaliação")
-    
-    return user_carona
+) -> UserCaronaModel:
+    # completar
+    return
